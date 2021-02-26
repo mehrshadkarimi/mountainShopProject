66f4904075dddc35765839b04e522bc1a17ca7cb
Mehrshad <mehrshad.karimi@yekpay.com>
2021-02-27 01:34:45 +0330

    Updates

---
+++ precache-manifest.286350fc396cf761efb4bbd8dfd97b9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26852c91793e9af5e38cb45a77ca9491",
+    "url": "/mountainShopProject/index.html"
+  },
+  {
+    "revision": "9e3baafc421cb07ed250",
+    "url": "/mountainShopProject/static/css/2.862b2f9f.chunk.css"
+  },
+  {
+    "revision": "186644a71957be3151b1",
+    "url": "/mountainShopProject/static/css/main.032d35ef.chunk.css"
+  },
+  {
+    "revision": "9e3baafc421cb07ed250",
+    "url": "/mountainShopProject/static/js/2.9c563ab3.chunk.js"
+  },
+  {
+    "revision": "186644a71957be3151b1",
+    "url": "/mountainShopProject/static/js/main.652b1827.chunk.js"
+  },
+  {
+    "revision": "9b7a8822c29db4e2238b",
+    "url": "/mountainShopProject/static/js/runtime~main.6f5cb020.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/mountainShopProject/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/mountainShopProject/static/media/shopping-bag.30f07071.svg"
+  }
+]);
